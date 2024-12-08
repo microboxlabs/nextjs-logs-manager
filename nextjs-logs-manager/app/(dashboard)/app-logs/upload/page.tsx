d705ff61d4950bfbdeeed9c402340b6d02378fc5
Enzo Briones <enzo.briones@inacapmail.cl>
2024-12-08 16:05:32 -0300

    refactor: Simplify layout structure in UploadPage and enhance UploadForm styling

--- nextjs-logs-manager/app/(dashboard)/app-logs/upload/page.tsx
+++ nextjs-logs-manager/app/(dashboard)/app-logs/upload/page.tsx
@@ -2,7 +2,7 @@ import UploadForm from "./ui/UploadForm";
 
 export default function UploadPage() {
   return (
-    <div className="flex min-h-screen items-center justify-center p-10">
+    <div className="flex items-center justify-center p-10">
       <UploadForm />
     </div>
   );
